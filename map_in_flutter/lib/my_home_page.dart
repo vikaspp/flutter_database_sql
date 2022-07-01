2f5daca5520a8ba56ee86130bfbbd1d1109b545f
Vikas <vikas.p@peerbits.com>
2022-07-01 14:16:09 +0530

    current location

---
+++ map_in_flutter/lib/my_home_page.dart
@@ -0,0 +1,99 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>  {
+
+  final Completer<GoogleMapController> _controller = Completer();
+  //GoogleMapController? _controller;
+//  final LatLng _center = const LatLng(22.9937067, 72.501555);
+
+  final List<Marker> _markers =  <Marker>[
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(28.644800, 77.216721),
+        infoWindow: InfoWindow(
+          title: 'marker tiote'
+        )
+    )
+  ];
+
+  static  CameraPosition _initPostion = CameraPosition(
+      target: LatLng(22.9937067, 72.501555),
+      zoom: 14,
+  );
+
+  Future<Position> getUserCurrentLocation() async {
+    await Geolocator.requestPermission().then((value){
+
+    }).onError((error, stackTrace)async{
+      await Geolocator.requestPermission();
+      print("Error"+error.toString());
+    });
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title:  Text('GoogleMap'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: GoogleMap(
+          onMapCreated: (GoogleMapController controller){
+            _controller.complete(controller);
+          },
+          markers: Set<Marker>.of(_markers),
+         initialCameraPosition: _initPostion,
+        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()async{
+          getUserCurrentLocation().then((value)async{
+            print("My current location");
+            print(value.latitude.toString() +" "+ value.longitude.toString());
+
+            _markers.add(
+              Marker(
+                  markerId: MarkerId('2'),
+                  position: LatLng(value.latitude,value.longitude),
+                  infoWindow: InfoWindow(
+                    title: 'My Current Location'
+                  )
+              )
+            );
+
+            CameraPosition cameraPosition1 = CameraPosition(
+                zoom: 25,
+                target: LatLng(value.latitude,value.longitude)
+            );
+            final GoogleMapController controller1 = await _controller.future;
+            controller1.animateCamera(CameraUpdate.newCameraPosition(cameraPosition1));
+           // controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+            setState((){
+
+            });
+          });
+        },
+        //elevation: 10,
+
+        child:  Icon(Icons.my_location),
+      ),
+      );
+
+  }
+}
+
